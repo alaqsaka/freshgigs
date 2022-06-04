e5cdbd8550c69c5047baed3532cb780d2242b578
alaqsaka <aqsaabidin15@gmail.com>
2022-06-04 16:05:45 +0700

    first commit

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,12 @@
+<h1>{{ $heading }}</h1>
+
+@unless (count($listings) == 0)
+    @foreach($listings as $listing)
+    <a href="/listings/{{ $listing['id'] }}">{{$listing['title']}}</a>
+    
+    <p>{{$listing['description']}}</p>
+    @endforeach
+@else
+    <p>no listings found</p>
+@endunless
+
