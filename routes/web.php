86c39388a39e0e2a30e78270a97ff9339e0b5fc3
alaqsaka <aqsaabidin15@gmail.com>
2022-07-16 17:35:26 +0700

    feat: adding new route for register, create user controller, create register page

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ListingController;
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -66,3 +67,6 @@ Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 // show Single listing (taro paling bawah)
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+// Show Register/create form 
+Route::get('/register', [UserController::class, 'create']);
