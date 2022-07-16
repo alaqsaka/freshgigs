caf06de76504715031d3c5124db00e67c45c1f49
alaqsaka <aqsaabidin15@gmail.com>
2022-07-16 20:34:43 +0700

    feat: edit navbar auth

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -42,6 +42,17 @@
                 freshgigs
             </a>
             <ul class="flex space-x-6 mr-6 text-lg">
+                @auth
+                <li>
+                    <span class="font-bold uppercase"> Welcome {{ auth()->user()->name }}</span>
+                </li>
+                <li>
+                    <a href="/listings/manage" class="hover:text-laravel"
+                        ><i class="fa-solid fa-gear"></i>
+                        Manage Listings</a
+                    >
+                </li>
+                @else
                 <li>
                     <a href="/register" class="hover:text-laravel"
                         ><i class="fa-solid fa-user-plus"></i> Register</a
@@ -53,6 +64,7 @@
                         Login</a
                     >
                 </li>
+                @endauth
             </ul>
         </nav>
 
